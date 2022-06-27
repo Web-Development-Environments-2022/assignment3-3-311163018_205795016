c802b05ccd5f5d025207b5d629456ef5479f9abd
hodisan94 <dhodisan@gmail.com>
2022-06-27 21:01:14 +0300

    background

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
+
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link :to="{ name: 'main' }">Vue Recipes</router-link>|
+    <!-- <div id="nav">
+      <router-link :to="{ name: 'main' }">Foodify</router-link>|
       <router-link :to="{ name: 'search' }">Search</router-link>|
       {{ !$root.store.username }}
       <span v-if="!$root.store.username">
@@ -12,8 +13,37 @@
       <span v-else>
         {{ $root.store.username }}: <button @click="Logout">Logout</button>|
       </span>
+    </div> -->
+    <div id="nav">
+      <ul class="nav">
+        <li class="nav-item">
+          <router-link :to="{ name: 'main' }">Foodify</router-link>
+
+          <!-- <a class="nav-link active" href="#">Active</a> -->
+        </li>
+        <li class="nav-item">
+          <router-link :to="{ name: 'search' }">Search</router-link>
+          {{ !$root.store.username }}
+        </li>
+        <li class="nav-item">
+          <span v-if="!$root.store.username">
+            Guest:
+            <router-link :to="{ name: 'register' }">Register</router-link>
+            <a></a>
+            <router-link :to="{ name: 'login' }">Login</router-link>
+          </span>
+          <span v-else>
+            {{ $root.store.username }}: <button @click="Logout">Logout</button>|
+          </span>
+        </li>
+        <li class="nav-item">
+
+        </li>
+      </ul>
     </div>
+
     <router-view />
+
   </div>
 </template>
 
@@ -33,6 +63,7 @@ export default {
 };
 </script>
 
+
 <style lang="scss">
 @import "@/scss/form-style.scss";
 
@@ -44,13 +75,24 @@ export default {
   min-height: 100vh;
 }
 
+
 #nav {
-  padding: 30px;
+  padding: 15px;
+  background: #5d7fdd;
+
 }
 
 #nav a {
   font-weight: bold;
   color: #2c3e50;
+}
+
+#nav li{
+  padding-left: 15px;
+}
+
+#nav li span a{
+  padding-left: 10px;
 }
 
 #nav a.router-link-exact-active {
